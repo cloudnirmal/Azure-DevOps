6fb1310942e737d9b9e955a627608d9b5ca6a023
cloudnirmal <123809774+cloudnirmal@users.noreply.github.com>
2024-08-03 21:30:39 +0530

    Update backend.tf

--- Infra-CICD-Terraform/TF-Files/backend.tf
+++ Infra-CICD-Terraform/TF-Files/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "azure-devops-learning"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "azuredevopsnirmal"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "picicdtfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "dev.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    resource_group_name  = "<your_resource_group_name>"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "<your_storage_account_name>"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "<your_storage-container_name>"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "<your_terraform_tfstate_file_name>"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
